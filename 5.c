4da9bbbe2b3417d11c5f90eae331e2e7e1b1e807
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-14 19:29:53 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,19 @@
+/*
+input:
+mani
+5
+
+output:
+mani won 5 medals
+*/
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char c[10];
+int i;
+scanf("%s %d",&c,&i);
+printf("%s won %d medals",c,i);
+}
