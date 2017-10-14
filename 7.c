94eb9210dba0f6e4576500b997d59ae1b142fe4b
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-14 20:35:30 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,15 @@
+/*initial amount of donation*/
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c,d;
+scanf("%d %d",&a,&b);
+d=a;
+for(c=0;c<b;c++){
+    d=d*2;
+    }
+printf("%d",d);
+}
