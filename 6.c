0202160d3758b29d053513a338f27e1f53e6aaae
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-14 19:43:48 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,17 @@
+/*input:
+name
+
+output:
+Hi name, Welcome to Taiwan!
+*/
+
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char a[12];
+scanf("%s",&a);
+printf("Hi %s, Welcome to Taiwan!",a);
+}
