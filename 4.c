dbf810f6884d1d1b26f8a7e29a48622f21844ecd
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-14 19:25:40 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,19 @@
+/*Railway time
+input:
+23
+12
+15
+
+output:
+23:12:15
+*/
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+printf("%d:%d:%d",a,b,c);
+}
