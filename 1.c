09fe6cb692edae40ceff0603bc927c2aba1802a9
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-14 18:57:24 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,24 @@
+//Test case
+input
+india
+delhi
+1498.871
+
+output
+The capital of India is Delhi and it's GDP is 1498.87//
+
+
+
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c[10],s[10];
+    float i;
+    scanf("%s %s %f",&c,&s,&i);
+    printf("The capital of %s is %s and it's GDP is %.2f",&c,&s,i);
+
+
+}
