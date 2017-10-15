97cd3b9e00269e8a9a3273b13a1b7273590bd452
manisgit <32795730+manisgit@users.noreply.github.com>
2017-10-15 11:32:56 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,22 @@
+/*
+input:
+300
+45
+
+output:
+30
+*/
+
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c,d,e;
+scanf("%d %d",&a,&b);
+c=a/b;
+d=b*c;
+e=a-d;
+printf("%d",e);
+}
